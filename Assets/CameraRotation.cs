82341855ddfb37ac10f8e6f1f6a3ce62c3a92bdc
Gajam_Jyothsna <jyothsna.gajam@yesgnome.com>
2022-02-24 16:02:51 +0530

    Camera Rotation is initialized.

---
+++ Assets/CameraRotation.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraRotation : MonoBehaviour
+{
+    public float rotationSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mousex, mousey;
+        
+        mousex = Input.GetAxis("Mouse X");
+       
+        mousey = Input.GetAxis("Mouse Y");
+
+        //transform.rotation = Quaternion.Euler(0, 90, 0) * Quaternion.Euler(0, 30, 0); //It always shows 120
+        transform.rotation = transform.rotation * Quaternion.Euler(-mousey+rotationSpeed, mousex+rotationSpeed , 0);
+        //transform.rotation = transform.rotation * Quaternion.Euler(mousex, 0, 0);
+    }
+}
